717b0d188ee33be4bfe7b06f594efe06b203e445
Galeno de Melo <galenodemelo@gmail.com>
2021-09-11 12:06:33 -0300

    feat: Adicionando menu de navegação

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.sass"
 export default function Header() {
     return (
         <header className={styles.header}>
-            <div className={[styles.headerBar, "grid-centered"].join(" ")}>
+            <div className={styles.bar}>
                 <div className={styles.logo}>
                     <a href="#">
                         <Image src="/img/elephant-skin-logo.svg" layout="fill" alt="Elephant Skin pink logo, composed by a lettering and an elephant icon at the end" />
@@ -14,6 +14,36 @@ export default function Header() {
                 <button className={styles.button}>
                     <Image src="/img/menu-animation.gif" layout="fill" alt="Bars animation growing and shrinking" />
                 </button>
+            </div>
+
+            <div className={styles.menu}>
+                <nav className={styles.nav}>
+                    <div>
+                        <a href="#" className={styles.link}>
+                            Hello
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#" className={styles.link}>
+                            Stories
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#" className={styles.link}>
+                            Moodboard
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#" className={styles.link}>
+                            Careers
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#" className={styles.link}>
+                            Contact
+                        </a>
+                    </div>
+                </nav>
             </div>
         </header>
     )
