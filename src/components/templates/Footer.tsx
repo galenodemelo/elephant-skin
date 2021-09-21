8107dbaed074381b2a99f9420c3ffab3f078c386
Galeno de Melo <galenodemelo@gmail.com>
2021-09-20 21:23:58 -0300

    feat: Criando lado direito do rodapé

--- src/components/templates/Footer.tsx
+++ src/components/templates/Footer.tsx
@@ -1,3 +1,37 @@
+import Image from "next/image"
+import Cities from "./Cities"
+import styles from "./Footer.module.sass"
+import HelperLinks from "./HelperLinks"
+import SocialLinks from "./SocialLinks"
+
+export default function Footer() {
+    return (
+        <section className={styles.footer}>
+            <div className={styles.background}>
+                <Image src="/img/decorative/world-map.png" layout="fill" objectFit="contain" objectPosition="left bottom" />
+            </div>
+
+            <div className={[styles.content, "grid-centered"].join(" ")}>
+                <div>
+                    <div className={styles.header}>
+                        <h2 className={styles.heading}>
+                            <small>Be An</small>
+                            <span>Elephant</span>
+                        </h2>
+                        <p className={styles.subheading}>
+                            Ready to work with us? Check it out the open positions!
+                        </p>
                         <a href="#" className="btn btn--pink">
                             Careers
                         </a>
+                    </div>
+                    <div className={styles.helper}>
+                        <SocialLinks />
+                        <Cities />
+                        <HelperLinks />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
