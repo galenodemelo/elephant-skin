84f0cf5cc6498700200b47042f7b4f32fe5ea27e
Galeno de Melo <galenodemelo@gmail.com>
2021-10-10 19:56:04 -0300

    feat: Adicionando animações dos textos

--- src/components/templates/NearestOffice.tsx
+++ src/components/templates/NearestOffice.tsx
@@ -17,7 +17,7 @@ export default function NearestOffice({triggerAnimation = true}: Props) {
                 5:12 pm
             </time>
             
-            <a href="#" className="btn btn--ghost">
+            <a href="#" className={[styles.allOffices, "btn", "btn--ghost"].join(" ")}>
                 All offices
             </a>
         </div>
