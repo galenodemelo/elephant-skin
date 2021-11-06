7c4327859a5146844d330140b747690437d5857f
Galeno de Melo <galenodemelo@gmail.com>
2021-11-06 20:06:26 -0300

    feat: Criando header

---
+++ src/components/pages/about-us/MeetTheElephants.tsx
@@ -0,0 +1,16 @@
+import styles from "./MeetTheElephants.module.sass"
+
+export default function MeetTheElephants() {
+    return (
+        <section className={styles.meetTheElephants}>
+            <div className="grid-centered">
+                <h2 className={styles.heading}>
+                    Meet <br />
+                    the  <br />
+                    elephants
+                </h2>
+
+            </div>
+        </section>
+    )
+}
