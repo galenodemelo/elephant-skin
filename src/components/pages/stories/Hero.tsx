2028c0f1d724d81b3237dfbf7d9a9cc47d783ea3
Galeno de Melo <galenodemelo@gmail.com>
2021-11-07 16:08:23 -0300

    refactor: Movendo o pictograma para um componente separado

--- src/components/pages/stories/Hero.tsx
+++ src/components/pages/stories/Hero.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Picto, { PictoColor } from "@components/animated/Picto"
 import styles from "./Hero.module.sass"
 
 export default function Hero() {
@@ -10,9 +10,7 @@ export default function Hero() {
                     <b>impact</b>
                 </h1>
 
-                <div className={styles.picto}>
-                    <Image src="/img/elephant-skin-picto-pink.svg" layout="fill" alt="X icon" />
-                </div>
+                <Picto color={PictoColor.PINK} />
             </div>
         </section>
     )
