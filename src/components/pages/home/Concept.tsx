336b52b264b7a10e591bca6da6c7923a5550b56c
Galeno de Melo <galenodemelo@gmail.com>
2021-12-06 20:59:55 -0300

    feat: Animando texto e adicionando autor

--- src/components/pages/home/Concept.tsx
+++ src/components/pages/home/Concept.tsx
@@ -15,10 +15,13 @@ export default function Concept() {
                     <Image src="/img/decorative/elephant-draw-concept.png" layout="fill" objectFit="contain" />
                 </div>
                 <div className={styles.content}>
-                    <div className={styles.text}>
+                    <div className={styles.text} data-play-on-active>
                         <p>To have an elephant skin means to be bold, sharp, think forward, and make it happen.</p>
                         <p>Our team is built of minds, not arms.</p>
                         <p>We are thinkers and doers that transform real estate into masterpieces!”</p>
+                        <p>
+                            <small><span style={{ textTransform: "uppercase" }}>Henrique Driessen</span> - Founder and CEO</small>
+                        </p>
                     </div>
                     <div className={styles.picto}>
                         <Image src="/img/elephant-skin-picto-white.svg" layout="fill" />
