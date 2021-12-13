84aae8aeca6bf5d822c40d4db95249d088954477
Galeno de Melo <galenodemelo@gmail.com>
2021-12-12 22:03:56 -0300

    fix: Corrigindo brush devido problema entre background-attachment: fixed e o Swiper

--- src/components/pages/home/Facade.tsx
+++ src/components/pages/home/Facade.tsx
@@ -2,20 +2,50 @@ import { useEffect, useRef } from "react"
 import Image from "next/image"
 import styles from "./Facade.module.sass"
 
-export default function Facade() {
+type Props = {
+    isActive?: boolean
+}
+
+export default function Facade({ isActive }: Props) {
     const brush = useRef<HTMLDivElement>(null)
+    const canvasElement = useRef<HTMLCanvasElement>(null)
     const cursorElement = useRef<HTMLDivElement>()
 
     useEffect(() => {
-        brush.current.addEventListener("mousemove", (event) => {
-            followCursor(event)
+        const ctx = canvasElement.current.getContext("2d")
+        const img = document.createElement("img")
+        img.src = "/img/decorative/facade-3d-model.jpg"
+        img.onload = () => {
+            ctx.drawImage(img, 0, 0, canvasElement.current.width, canvasElement.current.height)
+        }
 
-            const bubbles = document.createElement("span")
-            bubbles.style.left = (event.clientX - 140) + "px"
-            bubbles.style.top = (event.clientY - 140) + "px"
-            brush.current.appendChild(bubbles)
-        }, { passive: true })
-    }, [])
+        canvasElement.current.addEventListener("mousemove", reveal, { passive: true })
+    }, [isActive])
+
+    function reveal(event) {
+        const brushPosition = getBrush(event.clientX, event.clientY)
+        if (isActive) paint(brushPosition.x, brushPosition.y)
+    }
+
+    function getBrush(x, y) {
+        const canvasRect = canvasElement.current.getBoundingClientRect()
+        return {
+            x: Math.floor(
+                (x - canvasRect.left) / (canvasRect.right - canvasRect.left) * canvasElement.current.width
+            ),
+            y: Math.floor(
+                (y - canvasRect.top) / (canvasRect.bottom - canvasRect.top) * canvasElement.current.height
+            )
+        }
+    }
+
+    function paint(x, y) {
+        const ctx = canvasElement.current.getContext("2d")
+        ctx.beginPath()
+        ctx.arc(x, y, 180, 0, Math.PI * 2, true)
+        ctx.globalCompositeOperation = "destination-out"
+        ctx.fill()
+    }
 
     function followCursor(event: any) {
         const cursor = cursorElement.current
@@ -25,7 +55,8 @@ export default function Facade() {
 
     return (
         <section className={styles.facade}>
-            <div className={styles.brush} ref={brush}>
+            <div className={styles.brush} ref={brush} onMouseMove={followCursor}>
+                <canvas className={styles.image} ref={canvasElement} width={1000} height={1000}></canvas>
                 <div className={styles.cursor} ref={cursorElement}>
                     <Image src="/img/cursor/make-it-real.gif" layout="fill" />
                 </div>
