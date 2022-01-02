094968c89e4f9ff459f6dd8766e8f825d4ee78ab
Galeno de Melo <galenodemelo@gmail.com>
2022-01-02 17:09:56 -0300

    feat: Criando animação do lettering

--- src/components/pages/about-us/Intro.tsx
+++ src/components/pages/about-us/Intro.tsx
@@ -1,10 +1,28 @@
+import anime from "animejs"
 import Image from 'next/image'
+import { MutableRefObject, useEffect, useRef } from "react"
 import styles from "./Intro.module.sass"
 
-export default function Intro() {
+type Props = {
+    isActive?: boolean
+}
+
+export default function Intro({ isActive = false }: Props) {
+    const headingRef = useRef<HTMLHeadingElement>(null)
+
+    useEffect(() => {
+        if (isActive) {
+            if (!headingRef.current || headingRef.current.style.visibility == "visible") return
+            console.log("animating")
+            animateHello(headingRef)
+            headingRef.current.style.visibility = "visible"
+        }
+    }, [headingRef, isActive])
+
+
     return (
         <section className={styles.intro}>
-            <h1 className={styles.heading}>
+            <h1 className={styles.heading} ref={headingRef}>
                 Hello,
             </h1>
             <div className={styles.picto} data-play-on-active>
@@ -12,4 +30,21 @@ export default function Intro() {
             </div>
         </section>
     )
+}
+
+function animateHello(elementRef: MutableRefObject<any>) {
+    elementRef.current.innerHTML = elementRef.current.textContent.replace(/\S/g, `<span class="letter">$&</span>`);
+
+    const letterList = [].slice.call(elementRef.current.querySelectorAll(".letter"), 0);
+    anime.timeline()
+        .add({
+            targets: letterList,
+            translateY: [200,0],
+            scale: [0, 1],
+            translateZ: 0,
+            opacity: [0,1],
+            elasticity: 100,
+            duration: 3000,
+            delay: (el, i) => (150) * i
+        })
 }
