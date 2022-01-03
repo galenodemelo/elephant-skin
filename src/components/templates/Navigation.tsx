9007fcc6c7a028e0237d9781d8ed79e1393e12a5
Galeno de Melo <galenodemelo@gmail.com>
2022-01-03 16:16:31 -0300

    refactor: Movendo o menu para um componente isolado

---
+++ src/components/templates/Navigation.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link"
+import styles from "./Navigation.module.sass"
+
+const linkList: Array<{ name: string, link: string }> = [
+    { name: "Hello", link: "/aboutUs" },
+    { name: "Stories", link: "/stories" },
+    { name: "Moodboard", link: "#" },
+    { name: "Careers", link: "#" },
+    { name: "Contact", link: "/contact" },
+]
+
+type Props = {
+    isOpened: boolean,
+    setOpened: (opened: boolean) => void
+}
+
+export default function Navigation({ isOpened, setOpened }: Props) {
+    return (
+        <nav className={styles.nav}>
+            {linkList.map(({ name, link }, index) => {
+                return (
+                    <Link href={link} key={index}>
+                        <a className={styles.link} onClick={() => setOpened(false)}>
+                            {name}
+                        </a>
+                    </Link>
+                )
+            })}
+        </nav>
+    )
+}
